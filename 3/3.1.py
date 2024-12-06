c6f1b89b92a75b689226ba766c1ce6c61f0a0e74
evcann <evanholland1780@gmail.com>
2024-12-05 22:07:16 -0500

    Working day 3, HORRIBLE solution

---
+++ 3/3.1.py
@@ -0,0 +1,20 @@
+import re
+
+infile = open("3/input.txt", 'r')
+intext = infile.read()
+
+pattern = r"mul\(\d{1,3},\d{1,3}\)" # wow scary regex
+
+mul_statements = re.findall(pattern, intext)
+
+# print(mul_statements)
+
+def mul(a, b):
+    return a*b
+
+total = 0
+
+for statement in mul_statements:
+    total += eval(statement)
+
+print(total)
