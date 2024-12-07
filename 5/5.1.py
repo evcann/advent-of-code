261fd47970cae6dc6c39b840f33583c7dace5335
evcann <evanholland1780@gmail.com>
2024-12-06 23:28:29 -0500

    Correct 5.1 solution

---
+++ 5/5.1.py
@@ -0,0 +1,38 @@
+infile = open("5/input.txt", 'r')
+intext = infile.read()
+
+rules_unparsed, updates_unparsed = intext.split(sep="\n\n")[0].splitlines(), intext.split(sep="\n\n")[1].splitlines()
+
+rules = []
+for rule in rules_unparsed:
+    pair_strings = tuple(rule.split("|"))
+    pair = (int(pair_strings[0]), int(pair_strings[1]))
+    rules.append(pair)
+
+updates = []
+for update in updates_unparsed:
+    string_list = update.split(',')
+    int_list = []
+    for string in string_list:
+        int_list.append(int(string))
+    updates.append(int_list)
+
+passed_updates = []
+
+for update in updates:
+    passed = True # Default state is passing, set to false when a rule is violated
+    for rule in rules:
+        if rule[0] in update and rule[1] in update:
+            if not (update.index(rule[0]) < update.index(rule[1])):
+                passed = False
+                break
+    if passed:
+        passed_updates.append(update)
+
+total = 0
+
+for update in passed_updates:
+    middle = update[(len(update) // 2)]
+    total += middle
+
+print(total)
